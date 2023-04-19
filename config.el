2fa75d0a94789ef0cd1c126b9e6cbb5c4c970f36
colawithsauce <cola_with_sauce@foxmail.com>
2023-04-19 21:15:43 +0800

    first commit

---
+++ config.el
@@ -0,0 +1,147 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "colawithsauce"
+      user-mail-address "cola_with_sauce@foxmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "CaskaydiaCove Nerd Font Mono" :size 25)
+      doom-variable-pitch-font (font-spec :family "CaskaydiaCove Nerd Font" :size 25)
+      doom-unicode-font (font-spec :family  "LXGW WenKai Mono" :size 25))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'modus-vivendi-deuteranopia)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "/mnt/c/Users/colawithsauce/OneDrive/文档/Notes/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; (after! lsp-mode
+;;   (map! :leader
+;;         :desc "Diagnostics" "c-" #'lsp-ui-flycheck-list)
+;;   (setq +lsp-company-backends
+;;         (if (modulep! :editor snippets)
+;;             '(:separate company-capf company-yasnippet :with company-tabnine)
+;;           'company-capf)))
+;; ;; Completion settings
+;; (after! company
+;;   (setq company-idle-delay 0.250
+;;         company-minimum-prefix-length 1
+;;         company-show-quick-access t))
+
+(use-package org-protocol
+  :config
+  (add-to-list 'org-protocol-protocol-alist
+               '("org-find-file" :protocol "find-file" :function org-protocol-find-file :kill-client nil))
+
+  (defun org-protocol-find-file-fix-wsl-path (path)
+    "If inside WSL, change Windows-style paths to WSL-style paths."
+    (if (not (string-match-p "-[Mm]icrosoft" operating-system-release))
+        path
+      (save-match-data
+        (if (/= 0 (string-match "^\\([a-zA-Z]\\):\\(/.*\\)" path))
+            path
+          (let ((volume (match-string-no-properties 1 path))
+                (abspath (match-string-no-properties 2 path)))
+            (format "/mnt/%s%s" (downcase volume) abspath))))))
+
+  (defun org-protocol-find-file (fname)
+    "Process org-protocol://find-file?path= style URL."
+    (let ((f (plist-get (org-protocol-parse-parameters fname nil '(:path)) :path)))
+      (find-file (org-protocol-find-file-fix-wsl-path f))
+      (raise-frame)
+      (select-frame-set-input-focus (selected-frame)))))
+
+;; Cuda
+(add-to-list 'auto-mode-alist '("\\.cu\\'" . c++-mode))
+
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(require 'lsp-bridge)
+(global-lsp-bridge-mode)
+(setq lsp-bridge-c-lsp-server "clangd")
+(setq acm-enable-yas nil)
+(setq lsp-bridge-signature-show-function 'lsp-bridge-signature-posframe)
+(map!
+ :mode prog-mode
+ (:leader
+  :desc "Rename symbol" "cr" #'lsp-bridge-rename
+  :desc "Diagnostics list" "cx" #'lsp-bridge-diagnostic-list
+  :desc "Code actions" "ca" #'lsp-bridge-code-action
+  :desc "Format document" "cf" #'lsp-bridge-code-format))
+
+(add-to-list '+lookup-definition-functions #'lsp-bridge-find-def)
+(add-to-list '+lookup-implementations-functions #'lsp-bridge-find-impl)
+(add-to-list '+lookup-references-functions #'lsp-bridge-find-references)
+(add-to-list '+lookup-type-definition-functions #'lsp-bridge-find-type-def)
+(add-to-list '+lookup-documentation-functions #'lsp-bridge-popup-documentation)
+
+
+(unless (display-graphic-p)
+  (with-eval-after-load 'acm
+    (require 'acm-terminal)))
+
+(use-package! rime
+  :custom
+  (default-input-method "rime"))
