a14a2f6ee44b31dd80738c3a393301a004418994
colawithsauce <cola_with_sauce@foxmail.com>
2023-11-19 22:09:56 +0800

    Comment proxy settings.

--- config.el
+++ config.el
@@ -58,8 +58,8 @@
 (setq org-directory "~/Org/")
 
 
-(setq url-gateway-local-host-regexp
-      (concat "\\`" (regexp-opt '("localhost" "127.0.0.1")) "\\'"))
+;; (setq url-gateway-local-host-regexp
+;;       (concat "\\`" (regexp-opt '("localhost" "127.0.0.1")) "\\'"))
 ;; (setq url-proxy-services
 ;;       '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
 ;;         ("http" . "127.0.0.1:7890")
